2bed0a8eb60dfb10bba89df2a72ab1f81db7b647
Владислав Сороковых <black.walf16@gmail.com>
2026-05-18 22:13:33 +0300

    feat: описаны схемы Pydantic для ролей и пользователей

---
+++ backend/core/schemas/rbac.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, ConfigDict
+
+class RBACPermissions(BaseModel):
+    read_permission: bool | None = None
+    read_all_permission: bool | None = None
+    create_permission: bool | None = None
+    update_permission: bool | None = None
+    update_all_permission: bool | None = None
+    delete_permission: bool | None = None
+    delete_all_permission: bool | None = None
+
+    model_config = ConfigDict(extra='forbid')
