aeae42605c974d4f18adbe00fb75af6714178882
Владислав Сороковых <black.walf16@gmail.com>
2026-05-19 19:29:24 +0300

    fix: исправил констрейты БД

--- backend/core/database/models/users.py
+++ backend/core/database/models/users.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import BigInteger, String, ForeignKey, Boolean
+from sqlalchemy import BigInteger, String, ForeignKey, Boolean, CheckConstraint
 from sqlalchemy import Enum as SQLEnum
 
 from backend.core.constants import RoleName
@@ -31,11 +31,24 @@ class User(Base):
     team: Mapped[Team | None] = relationship(back_populates="members")
 
 
+ALLOWED_ROLES = ", ".join(f"'{role}'" for role in RoleName)
+
+
 class Role(Base):
     __tablename__ = "roles"
 
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
-    name: Mapped[RoleName] = mapped_column(
-        SQLEnum(RoleName, native_enum=False, length=50), unique=True
+    name: Mapped[str] = mapped_column(
+        SQLEnum(
+            RoleName,
+            native_enum=False,
+            length=50,
+            values_callable=lambda obj: [e.value for e in obj],
+        ),
+        unique=True,
     )
     users: Mapped[list["User"]] = relationship(back_populates="role", lazy="selectin")
+
+    __table_args__ = (
+        CheckConstraint(f"name IN ({ALLOWED_ROLES})", name="check_valid_role_name"),
+    )
