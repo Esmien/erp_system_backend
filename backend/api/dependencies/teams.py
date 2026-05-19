c07693e0154fe136e9595519641017171cd648aa
Владислав Сороковых <black.walf16@gmail.com>
2026-05-20 01:22:59 +0300

    docs: задокументированы модули с DI

--- backend/api/dependencies/teams.py
+++ backend/api/dependencies/teams.py
@@ -12,16 +12,34 @@ from backend.core.services.team_service import TeamService
 async def get_team_repo(
     session: AsyncSession = Depends(get_session),
 ) -> TeamRepository:
-    """Провайдер репозитория для инъекции в роутеры"""
+    """
+    Провайдер репозитория для работы с командами для инъекции в другие зависимости
+
+    Args:
+        session - сессия подключения к БД из пула
+
+    Returns:
+        Инстанс репозитория команд с проброшенной сессией
+    """
     return TeamRepository(session)
 
 
 async def get_team_service(
     repo: TeamRepository = Depends(get_team_repo),
 ) -> TeamService:
+    """
+    Провайдер сервиса для работы с командами для инъекции в Annotated
+
+    Args:
+        repo - репозиторий команд с проброшенной сессией
+
+    Returns:
+        Инстанс сервиса команд проброшенным репозиторием
+    """
     return TeamService(repo=repo)
 
 
+# Готовые DI для использования в роутерах
 TeamServiceDepends = Annotated[TeamService, Depends(get_team_service)]
 
 TeamCreateBody = Annotated[TeamCreate, Body()]
