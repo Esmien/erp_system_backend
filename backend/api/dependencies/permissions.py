3559db29c3591a75e61fb9d88cc0a68c9e44b29b
Владислав Сороковых <black.walf16@gmail.com>
2026-05-20 01:09:17 +0300

    docs: задокументирован permissions.py

--- backend/api/dependencies/permissions.py
+++ backend/api/dependencies/permissions.py
@@ -5,13 +5,14 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
 from backend.core.config import settings
-from backend.core.constants import RoleName
+from backend.core.constants import RoleName, BusinessElementName, PermissionName
 from backend.core.database.models.users import User
 from backend.exceptions import UserDoesNotExistsError, UserNotActiveError
 
 from backend.api.dependencies.reg_and_auth import AuthServiceDepends
 from backend.api.dependencies.rbac import RbacServiceDepends
 
+# Извлекает Bearer-токен из заголовка Authorization
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
@@ -19,23 +20,38 @@ async def get_current_user(
     token: str = Depends(oauth2_scheme),
     auth_service: AuthServiceDepends = None,
 ) -> User:
-    """Возвращает текущего активного пользователя по JWT токену"""
+    """
+    Возвращает текущего активного пользователя по JWT токену
+
+    Args:
+        token - JWT токен пользователя
+        auth_service - сервисный модуль для работы с аутентификацией
+
+    Returns:
+        Модель текущего авторизованного пользователя
+
+    Raises:
+        HTTPException(401) - при невалидном JWT
+    """
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Ошибка валидации токена",
         headers={"WWW-Authenticate": "Bearer"},
     )
 
+    # Декодируем токен и проверяем его валидность и вытаскиваем id пользователя
     try:
         payload = jwt.decode(
             token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
         )
         user_id = payload.get("sub")
+
         if user_id is None:
             raise credentials_exception
     except jwt.InvalidTokenError:
         raise credentials_exception
 
+    # Проверяем, активен ли пользователь (is_active=True)
     try:
         user = await auth_service.get_active_user_by_id(int(user_id))
         return user
@@ -43,8 +59,20 @@ async def get_current_user(
         raise credentials_exception
 
 
-async def get_admin_user(current_user: User = Depends(get_current_user)) -> User:
-    """Проверяет, является ли пользователь админом"""
+async def get_admin_user(current_user: "CurrentUserDepends") -> User:
+    """
+    Проверяет, является ли пользователь админом
+
+    Args:
+        current_user - текущий пользователь
+
+    Returns:
+        Модель пользователя, если он админ
+
+    Raises:
+        HTTPException(403) - если пользователь не является админом
+    """
+    # Проверяем, админ ли и выбрасываем исключение, если нет
     if current_user.role.name != RoleName.ADMIN:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
@@ -56,28 +84,46 @@ async def get_admin_user(current_user: User = Depends(get_current_user)) -> User
 class PermissionChecker:
     """Динамический чекер прав доступа через RBAC"""
 
-    def __init__(self, business_element: str, permission: str):
-        self.business_element = (
-            business_element.value
-            if hasattr(business_element, "value")
-            else business_element
-        )
-        self.permission = (
-            permission.value if hasattr(permission, "value") else permission
-        )
+    def __init__(
+        self, business_element: BusinessElementName, permission: PermissionName
+    ):
+        """
+        Инициализация чекера
+
+        Args:
+            business_element - ресурс, к которому проверяется доступ
+            permission - тип прав доступа (read, create, delete и т.д.)
+        """
+        self.business_element = business_element
+        self.permission = permission
 
     async def __call__(
         self,
-        user: User = Depends(get_current_user),
+        user: "CurrentUserDepends",
         rbac_service: RbacServiceDepends = None,
     ) -> User:
+        """
+        Делает класс вызываемым.
+        Выполняет проверку прав доступа к ресурсу
 
+        Args:
+            user - текущий пользователь
+            rbac_service - сервис проверки прав
+
+        Returns:
+            Возвращает модель пользователя, если прошел проверку
+
+        Raises:
+            HTTPException(403) - если не достаточно прав для доступа к ресурсам
+        """
+        # Проверка прав доступа для роли пользователя к ресурсу
         has_access = await rbac_service.check_permission(
             role_id=user.role_id,
             element_name=self.business_element,
             permission=self.permission,
         )
 
+        # Доступа нет
         if not has_access:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
