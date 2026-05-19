d00a720c7992cc910749e6e4f3d61fe271e58023
Владислав Сороковых <black.walf16@gmail.com>
2026-05-19 16:42:05 +0300

    feat: реализована модель команд

--- backend/core/database/models/__init__.py
+++ backend/core/database/models/__init__.py
@@ -1,6 +1,7 @@
 from backend.core.database.engine import Base
 from backend.core.database.models.users import User, Role
 from backend.core.database.models.rbac import BusinessElement, AccessRule
+from backend.core.database.models.teams import Team
 
 # Явное указание, что экспортируется из пакета
-__all__ = ["Base", "User", "Role", "BusinessElement", "AccessRule"]
+__all__ = ["Base", "User", "Role", "BusinessElement", "AccessRule", "Team"]
