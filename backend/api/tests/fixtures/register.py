362bf852811fd5606e0bcc86d846e97f95687c75
Владислав Сороковых <black.walf16@gmail.com>
2026-05-31 04:22:01 +0300

    refactor: общие поля JSON-моделей вынесены в отдельную фикстуру

--- backend/api/tests/fixtures/register.py
+++ backend/api/tests/fixtures/register.py
@@ -2,41 +2,47 @@ import pytest
 
 
 @pytest.fixture
-def valid_data_for_register():
+def base_user_data():
     return {
         "email": "user1@example.com",
         "name": "Иван",
         "surname": "Иванович",
         "last_name": "Иванов",
+    }
+
+
+@pytest.fixture
+def valid_data_for_register(base_user_data):
+    user_data = base_user_data.copy()
+    additional_fields = {
         "password": "secret_password",
         "repeat_password": "secret_password",
     }
+    user_data.update(**additional_fields)
+
+    return user_data
 
 
 @pytest.fixture
-def data_for_register_with_mismatch_passwords():
-    return {
-        "email": "user1@example.com",
-        "name": "Иван",
-        "surname": "Иванович",
-        "last_name": "Иванов",
-        "password": "secret_password",
-        "repeat_password": "secret_password1",
-    }
+def data_for_register_with_mismatch_passwords(valid_data_for_register):
+    invalid_data_for_register = valid_data_for_register.copy()
+    invalid_data_for_register["repeat_password"] = "another_password"
+
+    return invalid_data_for_register
 
 
 @pytest.fixture
-def success_register_response():
-    return {
-        "email": "user1@example.com",
-        "name": "Иван",
-        "surname": "Иванович",
-        "last_name": "Иванов",
+def success_register_response(base_user_data):
+    user_data = base_user_data.copy()
+    additional_fields = {
         "team_id": None,
         "id": 5,
         "is_active": True,
         "role": {"name": "user", "id": 3},
     }
+    user_data.update(**additional_fields)
+
+    return user_data
 
 
 @pytest.fixture
