431902141aadc8d776456182b4fcd3457846e816
Владислав Сороковых <black.walf16@gmail.com>
2026-07-06 04:25:03 +0300

    refactor: переписана логика возврата ошибок (статус-кодов)

--- backend/api/exception_handlers.py
+++ backend/api/exception_handlers.py
@@ -1,8 +1,11 @@
+from typing import Literal
+
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 from loguru import logger
 from sqlalchemy.exc import SQLAlchemyError
 
+from backend.core.utils.error_schemas import ErrorResponseSchema
 from backend.exceptions import (
     AccessDeniedError,
     BadCredentialsError,
@@ -29,48 +32,6 @@ from backend.exceptions import (
 )
 
 
-def not_found_exception_handler(request: Request, exc: Exception):
-    return JSONResponse(
-        status_code=status.HTTP_404_NOT_FOUND,
-        content={"detail": str(exc)},
-    )
-
-
-def internal_server_exception_handler(request: Request, exc: Exception):
-    return JSONResponse(
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        content={"detail": str(exc)},
-    )
-
-
-def not_authorized_exception_handler(request: Request, exc: Exception):
-    return JSONResponse(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        content={"detail": str(exc)},
-    )
-
-
-def access_denied_exception_handler(request: Request, exc: Exception):
-    return JSONResponse(
-        status_code=status.HTTP_403_FORBIDDEN,
-        content={"detail": str(exc)},
-    )
-
-
-def bad_request_exception_handler(request: Request, exc: Exception):
-    return JSONResponse(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        content={"detail": str(exc)},
-    )
-
-
-def conflict_exception_handler(request: Request, exc: Exception):
-    return JSONResponse(
-        status_code=status.HTTP_409_CONFLICT,
-        content={"detail": str(exc)},
-    )
-
-
 def sqlalchemy_exception_handler(request: Request, exc: Exception) -> JSONResponse:
     """
     Глобальный перехватчик любых непредвиденных ошибок базы данных.
@@ -80,35 +41,81 @@ def sqlalchemy_exception_handler(request: Request, exc: Exception) -> JSONRespon
     logger.error(
         f"Database error occurred while processing {request.method} {request.url.path} \nException details: {repr(exc)}"
     )
-
+    content = ErrorResponseSchema(
+        detail="Внутренняя ошибка базы данных. Пожалуйста, обратитесь в техническую поддержку.",
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
     # Отдаем клиенту безопасный стандартизированный ответ
     return JSONResponse(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        content={"detail": "Внутренняя ошибка базы данных. Пожалуйста, обратитесь в техническую поддержку."},
+        content=content.model_dump(),
     )
 
 
+# Единственная универсальная функция-фабрика
+def create_exception_handler(status_code: int, log_level: Literal["ERROR", "WARNING", "INFO", "DEBUG"] = "WARNING"):
+    def handler(request: Request, exc: Exception) -> JSONResponse:
+        log_message = f"Ошибка {status_code} при {request.method} {request.url.path} | Детали: {exc}"
+
+        logger.log(log_level, log_message)
+
+        content = ErrorResponseSchema(detail=str(exc), status_code=status_code)
+
+        return JSONResponse(
+            status_code=status_code,
+            content=content.model_dump(),
+        )
+
+    return handler
+
+
 def setup_exception_handlers(app: FastAPI):
-    app.add_exception_handler(TaskDoesNotExistError, not_found_exception_handler)
-    app.add_exception_handler(TeamDoesNotExistError, not_found_exception_handler)
-    app.add_exception_handler(RoleDoesNotExistError, internal_server_exception_handler)
-    app.add_exception_handler(AccessDeniedError, access_denied_exception_handler)
-    app.add_exception_handler(UserNotActiveError, access_denied_exception_handler)
-    app.add_exception_handler(TeamAlreadyExistError, bad_request_exception_handler)
-    app.add_exception_handler(UserAlreadyInTeamError, bad_request_exception_handler)
-    app.add_exception_handler(UserDoesNotExistError, bad_request_exception_handler)
-    app.add_exception_handler(UserExistsError, bad_request_exception_handler)
-    app.add_exception_handler(UserAlreadyActiveError, conflict_exception_handler)
-    app.add_exception_handler(BadCredentialsError, not_authorized_exception_handler)
-    app.add_exception_handler(PasswordsMismatchError, bad_request_exception_handler)
-    app.add_exception_handler(TaskNotCompletedError, bad_request_exception_handler)
-    app.add_exception_handler(TaskAlreadyEvaluatedError, bad_request_exception_handler)
-    app.add_exception_handler(MeetingOverlapError, conflict_exception_handler)
-    app.add_exception_handler(MeetingDoesNotExistError, not_found_exception_handler)
-    app.add_exception_handler(DatetimeMismatchError, bad_request_exception_handler)
-    app.add_exception_handler(UnknownAccessLevelError, internal_server_exception_handler)
-    app.add_exception_handler(InvalidPasswordError, access_denied_exception_handler)
-    app.add_exception_handler(CommentDoesNotExistsError, not_found_exception_handler)
-    app.add_exception_handler(EvaluationDoesNotExistError, not_found_exception_handler)
+    # Ошибки 404
+    app.add_exception_handler(TaskDoesNotExistError, create_exception_handler(status_code=status.HTTP_404_NOT_FOUND))
+    app.add_exception_handler(TeamDoesNotExistError, create_exception_handler(status_code=status.HTTP_404_NOT_FOUND))
+    app.add_exception_handler(MeetingDoesNotExistError, create_exception_handler(status_code=status.HTTP_404_NOT_FOUND))
+    app.add_exception_handler(
+        CommentDoesNotExistsError, create_exception_handler(status_code=status.HTTP_404_NOT_FOUND)
+    )
+    app.add_exception_handler(
+        EvaluationDoesNotExistError, create_exception_handler(status_code=status.HTTP_404_NOT_FOUND)
+    )
+
+    # Ошибки 401
+    app.add_exception_handler(BadCredentialsError, create_exception_handler(status_code=status.HTTP_401_UNAUTHORIZED))
+
+    # Ошибки 403
+    app.add_exception_handler(AccessDeniedError, create_exception_handler(status_code=status.HTTP_403_FORBIDDEN))
+    app.add_exception_handler(UserNotActiveError, create_exception_handler(status_code=status.HTTP_403_FORBIDDEN))
+    app.add_exception_handler(InvalidPasswordError, create_exception_handler(status_code=status.HTTP_403_FORBIDDEN))
+
+    # Ошибки 400
+    app.add_exception_handler(TeamAlreadyExistError, create_exception_handler(status_code=status.HTTP_400_BAD_REQUEST))
+    app.add_exception_handler(UserAlreadyInTeamError, create_exception_handler(status_code=status.HTTP_400_BAD_REQUEST))
+    app.add_exception_handler(UserDoesNotExistError, create_exception_handler(status_code=status.HTTP_400_BAD_REQUEST))
+    app.add_exception_handler(UserExistsError, create_exception_handler(status_code=status.HTTP_400_BAD_REQUEST))
+    app.add_exception_handler(PasswordsMismatchError, create_exception_handler(status_code=status.HTTP_400_BAD_REQUEST))
+    app.add_exception_handler(TaskNotCompletedError, create_exception_handler(status_code=status.HTTP_400_BAD_REQUEST))
+    app.add_exception_handler(
+        TaskAlreadyEvaluatedError, create_exception_handler(status_code=status.HTTP_400_BAD_REQUEST)
+    )
+    app.add_exception_handler(DatetimeMismatchError, create_exception_handler(status_code=status.HTTP_400_BAD_REQUEST))
+
+    # Ошибки 409
+    app.add_exception_handler(UserAlreadyActiveError, create_exception_handler(status_code=status.HTTP_409_CONFLICT))
+    app.add_exception_handler(MeetingOverlapError, create_exception_handler(status_code=status.HTTP_409_CONFLICT))
+
+    # Ошибки 500 (внутренние доменные)
+    app.add_exception_handler(
+        RoleDoesNotExistError,
+        create_exception_handler(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, log_level="ERROR"),
+    )
+    app.add_exception_handler(
+        UnknownAccessLevelError,
+        create_exception_handler(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, log_level="ERROR"),
+    )
+    app.add_exception_handler(
+        UnexpectedError, create_exception_handler(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, log_level="ERROR")
+    )
+
     app.add_exception_handler(SQLAlchemyError, sqlalchemy_exception_handler)
-    app.add_exception_handler(UnexpectedError, internal_server_exception_handler)
